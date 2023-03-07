8d22f81cb3343d00508b71122a03fb57a735071d
dnv5218 <91994528+dnv5218@users.noreply.github.com>
2023-03-07 13:48:06 -0600

    incomplete 7 march
    car and obstacles

---
+++ Mod06A-Velasco/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float TurnSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime*speed); //returns meters/second
+        transform.Translate(Vector3.right * Time.deltaTime * TurnSpeed);
+
+    }
+}
